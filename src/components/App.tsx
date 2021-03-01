8595bc7e61573e742ff52477ad8951f1f35814d8
DmitriiArbatskii <Dmitrii_Arbatskii@epam.com>
2021-03-01 14:03:47 +0300

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import '../styles/App.css'
+export const App: React.FC<{}> = () => (
+    <h1>Hello React</h1>
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index-bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
